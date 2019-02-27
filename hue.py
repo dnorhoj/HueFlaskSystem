3439299acf07dd2cc662625eb44f131f3c609a8d
dnorhoj <11206485+dnorhoj@users.noreply.github.com>
2019-02-27 21:16:55 +0100

    Felt wrong to call it OAuth without the 2

--- hue.py
+++ hue.py
@@ -4,7 +4,7 @@ BASE_API = "https://api.meethue.com"
 BRIDGE_API = f"{BASE_API}/bridge"
 OAUTH_API = f"{BASE_API}/oauth2"
 
-def generateOAuthURL():
+def generateAuthURL():
     hue = config.getConfig("hue")
     return f"{OAUTH_API}/auth?clientid={hue['clientid']}&appid={hue['appid']}&deviceid={hue['appid']}&state={hue['state']}&response_type=code"
 
