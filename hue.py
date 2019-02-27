947986e2605fcacfc97bb7b7aab1d071a3e6a7e6
dnorhoj <11206485+dnorhoj@users.noreply.github.com>
2019-02-27 21:11:08 +0100

    Baby steps (I didn't have a lot of time today)

--- hue.py
+++ hue.py
@@ -1,11 +1,12 @@
 import requests, config
 
-BASE_API = "https://api.meethue.com",
-BRIDGE_API = f"{BASE_API}/bridge",
+BASE_API = "https://api.meethue.com"
+BRIDGE_API = f"{BASE_API}/bridge"
 OAUTH_API = f"{BASE_API}/oauth2"
 
 def generateOAuthURL():
-    return "{base}/auth?clientid={clientid}&appid={appid}&deviceid={deviceid}&state={state}&response_type=code".format()
+    hue = config.getConfig("hue")
+    return f"{OAUTH_API}/auth?clientid={hue['clientid']}&appid={hue['appid']}&deviceid={hue['appid']}&state={hue['state']}&response_type=code"
 
 def authflow():
     pass
