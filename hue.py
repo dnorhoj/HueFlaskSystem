ea81f95a762b55f6a647febcba21070ce13f8c24
Daniel Nielsen <11206485+dnorhoj@users.noreply.github.com>
2019-02-28 12:25:45 +0100

    I added some functionality to the getToken method

--- hue.py
+++ hue.py
@@ -1,12 +1,29 @@
-import requests, config
+import requests, config, base64
 
 BASE_API = "https://api.meethue.com"
 BRIDGE_API = f"{BASE_API}/bridge"
 OAUTH_API = f"{BASE_API}/oauth2"
 
 def generateAuthURL():
-    hue = config.getConfig("hue")
-    return f"{OAUTH_API}/auth?clientid={hue['clientid']}&appid={hue['appid']}&deviceid={hue['appid']}&state={hue['state']}&response_type=code"
+	hue = config.getConfig("hue")
+	return f"{OAUTH_API}/auth?clientid={hue['clientid']}&appid={hue['appid']}&deviceid={hue['appid']}&state={hue['state']}&response_type=code"
 
-def authflow():
-    pass
+def getToken(code):
+	hue = config.getConfig("hue")
+
+	url = f"{OAUTH_API}/token"
+	authstring = f"{hue['clientid']}:{hue['clientsecret']}"
+	authstring = base64.b64encode(authstring.encode()).decode()
+
+	querystring = {
+		"code":code, 
+		"grant_type":"authorization_code"
+	}
+	
+	headers = {
+		'authorization': f'Basic {authstring}'
+	}
+
+	response = requests.request("POST", url, headers=headers, params=querystring)
+	
+	return response.json()
