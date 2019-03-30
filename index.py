c2235de9e31a648b89b461c416c5bfb3b4d4bad1
Daniel Nielsen <11206485+dnorhoj@users.noreply.github.com>
2019-03-30 15:57:42 +0100

    removed annoying print() statementrs

--- index.py
+++ index.py
@@ -21,7 +21,6 @@ def loginsite():
 		
 		login = auth.login(form('uname'), form('pass'))
 		if login[0]:
-			print(login)
 			session['userID'] = login[1]
 			return redirect(url_for('controlpanel'))
 		
@@ -64,7 +63,6 @@ def controlpanel():
 
 	error = request.args.get('error', "0")
 	h = Hue()
-	print(h.getData(session.get("userID")))
 	return render_template(
 		"controlpanel.html",
 		data=h.getData(session.get("userID")),
